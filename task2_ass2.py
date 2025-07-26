5db0b545fdecbd991ecfce39a5a9c766f9080fee
Ankit6174-cmd <tiwari.ankit225@gmail.com>
2025-07-26 17:18:41 +0530

    Add files via upload

---
+++ task2_ass2.py
@@ -0,0 +1,5 @@
+a = 0  # Initialize the sum variable
+# Loop through numbers from 1 to 50
+for i in range(1, 51):
+    a += i  # Add each number to the sum
+print("The sum of numbers from 1 to 50 is:",a)
